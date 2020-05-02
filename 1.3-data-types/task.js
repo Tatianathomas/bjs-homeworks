a360b812b7be87e867fd4c37170c5c18b6cf49d9
Tatiana Thomas <murzik555@inbox.ru>
2020-05-02 12:19:49 +0500

    third homework

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,29 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+if (isNaN(percent)) {
+  return `Параметр percent содержит неправильное значение ${percent}`;
 }
+if (isNaN(contribution)) {
+    return `Параметр contribution содержит неправильное значение ${contribution}`;
+} 
+if (isNaN(contribution)) {
+    return `Параметр amount содержит неправильное значение ${amount}`;
+} 
+if (isNaN(date)) {
+    return `Параметр date содержит неправильное значение ${date}`;
+} 
+const sumBack = amount - contribution;
+date = (new Date(date).getFullYear() - new Date().getFullYear()) * 12;
+const conditions = sumBack * (percent / 1200 + percent / 1200 / (((1 + percent / 1200)** date) - 1));
+let totalAmount = conditions * date;
+return parseFloat(totalAmount.toFixed(2));
+}
+console.log(calculateTotalMortgage(10, 0, 50000, "2021-05-02"));
+
+
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
-}
+    let greeting;
+    return greeting = `Привет, мир! Меня зовут ${name || "Аноним"}`;
+  }
+  console.log(getGreeting(""));
