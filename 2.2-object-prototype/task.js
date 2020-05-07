bc3f6d29f1015047c8a642a4b34a63390ca41f8c
Tatiana Thomas <murzik555@inbox.ru>
2020-05-07 21:24:30 +0500

    Homework to prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,28 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
-}
+    return (animal === undefined) ? null: animal.sound;
+    //animal === undefined ? return null: return sound;  
+//Эдгар, а почему не работает закоментированный код?
+  }
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks.length === 0) {
+      return 0;
+    }
+    let sum = 0; 
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+  }
+    const average = sum / marks.length;
+    const roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+
+  const now = Number(new Date());
+  birthday = Number(new Date(birthday));
+  const diff = now - birthday;
+  const age = diff / (365.25 * 24 * 60 * 60 * 1000); 
+  return age > 18;
 }
